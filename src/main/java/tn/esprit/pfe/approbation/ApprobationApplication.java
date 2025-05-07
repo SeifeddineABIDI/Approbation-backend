0c9541969bc91f62ed06c6b4f72761094e7dc678
Seifeddine ABIDI <seifeddine.abidi@esprit.tn>
2025-05-07 09:21:04 +0100

    Edited GitHub Actions CI workflow for backend

--- src/main/java/tn/esprit/pfe/approbation/ApprobationApplication.java
+++ src/main/java/tn/esprit/pfe/approbation/ApprobationApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ApprobationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApprobationApplication.class, args);
     }
